65f6323fa691d113f71a5ecb7487a0ce258310f4
최영돈(YeongDon, Choi) <80868508+ydonchoi@users.noreply.github.com>
2024-05-20 22:23:49 +0900

    내용 수정

--- bj_5615_아파트임대_230517.py
+++ bj_5615_아파트임대_230517.py
@@ -1,13 +1,46 @@
-#코딩 #파이썬 #백준 #문제풀이 #5615번(아파트임대문제)
+# 문제번호: 5615
+# 아파트임대
+# url: https://www.acmicpc.net/problem/5615
+# 23.05.17.
 
-a=list(map(int,input('면적 입력:').split(',')))
-judge,sol,cnt=[],{},0
-for k in a:
-    for x in range(1,k):
-        for y in range(1,k):
-            if k==(2*x*y)+x+y:
-                sol.update({k:(x,y)})
-                judge.append(True)
-    if len(judge) >= 1 : judge = []
-    else: cnt+=1
-print(f'없는 면적의 수: {cnt}\n면적별 x, y의 값: {sol}')
+#########################
+## (문제)
+## 동규 부동산에서는 아파트를 임대하고 있는데, 아파트의 방은 면적이 2xy+x+y (x, y > 0)임
+## 동규 부동산의 카탈로그에는 아파트 면적이 오름차순으로 적혀 있으며, 일부는 있을 수 없는 크기의 아파트도 적혀 있음
+## 있을 수 없는 크기의 아파트를 임대하려고 하면, 동규는 "꽝!"이라고 외치면서 수수료만 떼어감
+## 동규 부동산의 카탈로그에서 아파트 면적이 주어질 떄, 있을 수 없는 아파트의 수를 구하는 프로그램을 작성하시오.
+#########################
+
+def create_lent_catalogue(num):
+    area_list = [int(input('임대면적:')) for _ in range(num)]
+    return area_list
+
+def is_able_to_rent(area_list):
+    lentable_catalogue = {}
+    for area in area_list:
+        for x in range(1, area):
+            if int((area-x)/(2*x+1)) > 0:
+                lentable_catalogue.update({area: (x, int((area-x)/(2*x+1)))})
+    return lentable_catalogue
+
+def lent_apartment(area_list, lentable_catalogue):
+    unable_to_lent = 0
+    for area in area_list:
+        if not area in lentable_catalogue.keys():
+            unable_to_lent += 1
+            print('꽝!!')
+        else:
+            print("this is an apartment to lent. would you lent?")
+    return unable_to_lent
+        
+def main():
+    nums = int(input('아파트 수:'))
+    area_list = create_lent_catalogue(nums)
+    lentable_catalogue = is_able_to_rent(area_list)
+    unable_to_lent = lent_apartment(area_list, lentable_catalogue)
+    print(f'있을 수 없는 아파트 면적의 수: {unable_to_lent}')
+
+if __name__ == '__main__':
+    main()
+
+# fin.
