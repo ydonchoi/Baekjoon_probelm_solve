9fda93dc9763a8732b0cc5ca773dc86f92710b3d
최영돈(YeongDon, Choi) <80868508+ydonchoi@users.noreply.github.com>
2024-06-15 20:42:52 +0900

    Update_240615_3rd commit

--- bj_5525_.py
+++ bj_5525_.py
@@ -17,22 +17,25 @@
 '''
 (approach)
 1. start point에서 I와 O가 교대로 나오는 문자열을 탐색한다
-2. 교대로 출현하는 길이(len(PN)를 구하고, PN에서 O의 개수를 센다 -> PN
+2. 교대로 출현하는 길이(len(PN)를 구하고, PN에서 O의 개수를 센다 -> PN을 list로 반환받아 확인하고, len(PN)을 출력한다.
 3. 종료된 시점으로 start point를 옮긴 후, 다시 1번과 2번 과정을 반복한다.
 4. 탐색 indicator가 S 문자열 끝에 도착하면 종료한다.
 '''
 
 # My solution_revised(24.06.15.)
 def gen_sentence(num: int) -> str:
+    ''' input the length of sentence and return sentence composed to str 'I' or 'O' as long as the length of it '''
     return ''.join([random.choice(['I','O']) for _ in range(num)])
 
 def search_IOI(S: str, indicator: str = 'IO') -> list:
+    ''' search the whole sentence and return sentences with 'IOI' as type of list.
+        be able to set indicator 'IO', 'IOIO', 'IOIOIO', and more. '''
     start = 0
     idx = 0
     PN = []
     while idx < len(S):
-        if S[idx:idx+3] == indicator+'I':
-            idx += 2
+        if S[idx:idx+len(indicator)+1] == indicator+'I':
+            idx += len(indicator)
         else:
             if (idx-start) > 1:
                 PN.append(S[start:idx+1])
