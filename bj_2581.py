ae7cb90165fb7792958b516fef7c6e9efb485963
ydonchoi <ydonchoi@gmail.com>
2024-06-23 22:43:47 +0900

    update_240623

--- bj_2581.py
+++ bj_2581.py
@@ -3,6 +3,7 @@
 # url: https://www.acmicpc.net/problem/2581
 # 출처: 나의 네이버 블로그 (blog.naver.com/ydonchoi83)
 # 2022.06.19.~2022.07.01.
+# Revised on 2024. 06. 23.
 
 #########################
 # [문제]
@@ -10,25 +11,40 @@
 # 이들 소수의 합과 최소값을 찾는 프로그램을 작성하시오.
 #########################
 
-# be about to revise soon... Below is a prior solution.
+''' I knew old expression using Eratosthenes's sieve.
+    And this time, I have learnt two ways more efficient.
+    So.. update adopting these ways '''
 
+def is_prime1(num):
+    ''' the first way to check prime numbers, i.e. Eratosthenes's sieve '''
+    for x in range(2, num):
+        if num % x == 0:
+            return False
+    return True
 
-import random
-def summary_all_primes_btw():
-	m = random.randint(1, 100)
-	n = random.randint(1, 100)
-	print(f'{m}과 {n} 사이에 존재하는 소수는 ', end=' ')
-	sieve = [False, False] + [True]*(n-1)
-	primes = []
-	if m < n:
-		for i in range(m, n+1):
-			if sieve[i]:
-				primes.append(i)
-				for j in range(i*2, n+1, i):
-					sieve[j] = False
-		print(primes)
-		return print('*합: {}, *최소값: {}, *최대값: {}'.format(sum(primes), min(primes), max(primes)))
-summary_all_primes_btw()
+def is_prime2(num):
+    ''' the second way to check prime numbers, i.e. Eratosthenes's sieve '''
+    if num == 1 or num <= 0:
+        return False
+    for x in range(2, floor(sqrt(num))+1):
+        if num % x == 0:
+            return False
+    return True
 
+def main():
+    print('you input two integers "m" and "n"(m<n) with one space between them)')
+    m, n = map(int, input('input two integers: ').split())
+    nums = range(m, n+1)
+    primes = [x for x in nums if is_prime1(x)]
+    primes2 = [x for x in nums if is_prime2(x)]
+
+    print(f'sum : {sum(primes)} | min : {min(primes)} | len : {len(primes)}')
+    print(f'sum : {sum(primes2)} | min : {min(primes2)} | len : {len(primes2)}\n')
+    print(primes'\n')
+    print(primes2)
+
+if __name__ == "__main__":
+    from math import floor, sqrt
+    main()
 
 # fin.
