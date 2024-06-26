2ca3381a281d4dae5c5260a9c780fbba60f585fc
최영돈(YeongDon, Choi) <80868508+ydonchoi@users.noreply.github.com>
2024-06-26 21:46:52 +0900

    update_240626

--- 26day_word_search_230404.py
+++ 26day_word_search_230404.py
@@ -1,18 +1,22 @@
 # 26일차: 영어 단어 검색하기1_최영돈(문겸)
-print('영어 단어검색 v.0.1')
-while True:
-    myDic={'sky':'하늘','apple':'사과','can':'깡통','boat':'배','sea':'바다','smile':'미소','animal':'동물','park':'공원'}
-    search=[]
-    word=input('단어입력:')
-    if word == '':
-        print('탈출! yeap!')
-        break
-    else:
-        for key in myDic.keys():
-            if word == key:
-                search.append(word)
-                print(f'{word}의 뜻은 {myDic[word]}입니다.')
-                pass
-            else: pass
-        if search == []:
-            print(f'{word} 단어는 단어장에 없음')
+print('Eng_word search v0.1')
+def enroll_word(myDic: dict, word: str) -> dict:
+    meaning = input(f'enter the meaning of the {word}:')
+    myDic[word] = meaning
+    return myDic
+
+def main():
+    myDic = {}
+    test = int(input('testing number:'))
+    for _ in range(test):
+        word = input('enter a word:')
+        if word in myDic.keys():
+            print(f'The {word} means {myDic[word]}.')
+        else:
+            print('there is not in my dic.')
+            yn = input(f'Do you want to enroll the {word}? (y/n):').lower()
+            if yn[0] == 'y':
+                myDic = enroll_word(myDic, word)
+
+if __name__ == "__main__":
+    main()
